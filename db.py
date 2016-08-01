bbff75f858bfe7d3ffb53aaad732efc09560f852
Taylor Burgess <tayloraburgess@gmail.com>
2016-07-31 23:53:25 -0400

    Modify db.py to properly connect to Heroku db

--- db.py
+++ db.py
@@ -1,11 +1,21 @@
 import sys
 from os import system
 import psycopg2
+import urlparse
 import os
 
 def check_heroku_db():
     if 'CLEARDB_DATABASE_URL' in os.environ and os.environ['CLEARDB_DATABASE_URL']:
-        conn = psycopg2.connect(database=os.environ['CLEARDB_DATABASE_URL'])
+        urlparse.uses_netloc.append("postgres")
+        url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+        conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
         return conn
     else:
         conn = psycopg2.connect(database="expenses")
