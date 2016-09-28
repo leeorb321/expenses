b317d579ad7ad1aa0aafb87b764b41fc468cdd65
Taylor Burgess <tayloraburgess@gmail.com>
2016-09-28 15:23:39 -0400

    Add basic bootstrap-table structure

--- backend/expenses_app.py
+++ backend/expenses_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, make_response
+from flask import Flask, send_file, request, redirect, make_response, jsonify
 from os import system
 from datetime import datetime
 from db import *
@@ -7,101 +7,100 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return redirect('/retrieve_data')
+    return send_file('../build/index.html')
 
-@app.route('/submit_expense', methods=['GET','POST'])
-def submit_page():
-    if request.method == 'GET':
-        persons, categories = connect()
-        return render_template(
-            'submit_expense.html',
-            categories=categories,
-            persons=persons
-        )
-    elif request.method == 'POST':
-        person = (request.form['person']).lower()
-        expense_date = request.form['expense_date']
-        amount = int(float(request.form['amount'])*100)
-        category = (request.form['category']).lower()
-        description = request.form['description']
+# @app.route('/submit_expense', methods=['GET','POST'])
+# def submit_page():
+#     if request.method == 'GET':
+#         persons, categories = connect()
+#         return render_template(
+#             'submit_expense.html',
+#             categories=categories,
+#             persons=persons
+#         )
+#     elif request.method == 'POST':
+#         person = (request.form['person']).lower()
+#         expense_date = request.form['expense_date']
+#         amount = int(float(request.form['amount'])*100)
+#         category = (request.form['category']).lower()
+#         description = request.form['description']
 
-        new_expense(person, expense_date, amount, category, description)
+#         new_expense(person, expense_date, amount, category, description)
 
-        return redirect('/')
+#         return redirect('/')
 
-@app.route('/add_category', methods=['POST'])
-def add_category():
-    category = (request.form['category_name']).lower()
+# @app.route('/add_category', methods=['POST'])
+# def add_category():
+#     category = (request.form['category_name']).lower()
 
-    new_category(category)
+#     new_category(category)
 
-    return redirect(request.referrer)
+#     return redirect(request.referrer)
 
-@app.route('/update_entry', methods=['POST'])
-def update_entry():
-    new_person = request.form['person']
-    new_date = request.form['date']
-    new_amount = int(request.form['amount'].replace('.','').replace('$',''))
-    new_category = request.form['category']
-    new_description = request.form['description']
-    txn_id = request.form['txn_id']
+# @app.route('/update_entry', methods=['POST'])
+# def update_entry():
+#     new_person = request.form['person']
+#     new_date = request.form['date']
+#     new_amount = int(request.form['amount'].replace('.','').replace('$',''))
+#     new_category = request.form['category']
+#     new_description = request.form['description']
+#     txn_id = request.form['txn_id']
 
-    update_expense(new_person, new_date, new_amount, new_category, new_description, txn_id)
+#     update_expense(new_person, new_date, new_amount, new_category, new_description, txn_id)
 
-    return make_response()
+#     return make_response()
 
-@app.route('/delete_entry', methods=['DELETE'])
-def delete_entry():
-    txn_id = request.form['txn_id']
+# @app.route('/delete_entry', methods=['DELETE'])
+# def delete_entry():
+#     txn_id = request.form['txn_id']
 
-    delete_expense(txn_id)
+#     delete_expense(txn_id)
 
-    return make_response()
+#     return make_response()
 
 @app.route('/retrieve_data', methods=['GET'])
 def retrieve_data():
     persons, categories = connect()
     data = display_all_data()
 
-    return render_template(
-        'results.html',
-        data=data,
-        persons=persons,
-        categories=categories
-    )
+    return jsonify({
+        'data': data,
+        'persons': persons,
+        'categories': categories
+    })
 
-@app.route('/visualize', methods=['GET'])
-def visualize():
-    persons, categories = connect()
-    data = display_all_data()
+# @app.route('/visualize', methods=['GET'])
+# def visualize():
+#     persons, categories = connect()
+#     data = display_all_data()
 
-    if request.method == 'GET':
-        chart_type = 'pie'
-        date_from = 'None'
-        date_to = 'None'
-        selected_categories = categories
-        selected_persons = persons
-        display = 'category'
+#     if request.method == 'GET':
+#         chart_type = 'pie'
+#         date_from = 'None'
+#         date_to = 'None'
+#         selected_categories = categories
+#         selected_persons = persons
+#         display = 'category'
 
-    return render_template(
-        'visualize.html',
-        data=data,
-        categories=categories,
-        persons=persons,
-        chartType=chart_type,
-        dateFrom = date_from,
-        dateTo = date_to,
-        categoriesSelected=selected_categories,
-        personsSelected=selected_persons,
-        display=display
-    )
+#     return render_template(
+#         'visualize.html',
+#         data=data,
+#         categories=categories,
+#         persons=persons,
+#         chartType=chart_type,
+#         dateFrom = date_from,
+#         dateTo = date_to,
+#         categoriesSelected=selected_categories,
+#         personsSelected=selected_persons,
+#         display=display
+#     )
 
-@app.route('/')
-def backup_db():
-    backup_name = str(datetime.now()).replace(':','-')
-    bu_command = 'pg_dump expenses > %s' % backup_name
-    system(bu_command)
-    redirect(request.referrer)
+# @app.route('/')
+# def backup_db():
+#     backup_name = str(datetime.now()).replace(':','-')
+#     bu_command = 'pg_dump expenses > %s' % backup_name
+#     system(bu_command)
+#     redirect(request.referrer)
 
 if __name__ == '__main__':
     if 'HEROKU_CHECK' in os.environ:
--- src/App.js
+++ src/App.js
@@ -1,18 +1,78 @@
 import React, { Component } from 'react';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import logo from './logo.svg';
+import $ from 'jquery';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+
+const retrieveDataAPI = 'retrieve_data'
+
+var ViewExpenses = React.createClass({
+  getInitialState: function() {
+    return {
+      categories: [],
+      data: [],
+      persons: []
+    }
+  },
+
+  componentDidMount: function() {
+    $.ajax({
+        url: retrieveDataAPI,
+        dataType: 'json',
+        cache: false,
+        success: function(resData) {
+            this.setState({
+              categories: resData.categories,
+              data: resData.data,
+              persons: resData.persons
+            });
+        }.bind(this),
+        error: function(xhr, status, err) {
+            //console.error(this.props.niceties, status, err.toString());
+        }.bind(this)
+    });
+  },
+
+  render: function() {
+    const tableData = this.state.data.map((dataPoint) => {
+      return {
+        id: dataPoint.id,
+        date: dataPoint.date,
+        amount: dataPoint.amount,
+        category: dataPoint.category,
+        person: dataPoint.person,
+        description: dataPoint.description
+      }
+    });
+
+    const editObject = {
+      mode: 'click',
+      blurToSave: true
+    }
+    const tableOptions = {
+      clearSearch: true,
+      paginationSize: 25
+    }
+    return (
+      <BootstrapTable data={tableData} striped={true} hover={true} search={true} cellEdit={editObject} columnFilter={true} pagination={true} options={tableOptions}>
+        <TableHeaderColumn dataField="id" isKey={true} dataAlign="center" dataSort={true} editable={false}>ID</TableHeaderColumn>
+        <TableHeaderColumn dataField="date" dataSort={true}>Date</TableHeaderColumn>
+        <TableHeaderColumn dataField="amount" dataSort={true}>Amount</TableHeaderColumn>
+        <TableHeaderColumn dataField="category" dataSort={true}>Category</TableHeaderColumn>
+        <TableHeaderColumn dataField="person" dataSort={true}>Person</TableHeaderColumn>
+        <TableHeaderColumn dataField="description">Description</TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ViewExpenses />
       </div>
     );
   }
